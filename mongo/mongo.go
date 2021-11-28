1301eaedb4bc0f1821c1e1f0d900f0e39d3e15ad
Alton <28844720+Altonhe@users.noreply.github.com>
2021-11-28 11:59:29 +0800

    mongo: initial implementation (#10)
    Co-authored-by: Joe Chen <jc@unknwon.io>

---
+++ mongo/mongo.go
@@ -0,0 +1,183 @@
+package mongo
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/flamego/cache"
+)
+
+var _ cache.Cache = (*mongoStore)(nil)
+
+// mongoStore is a MongoDB implementation of the cache store.
+type mongoStore struct {
+	nowFunc    func() time.Time // The function to return the current time
+	db         *mongo.Database  // The database connection
+	collection string           // The database collection for storing cache Data
+	encoder    cache.Encoder    // The encoder to encode the cache Data before saving
+	decoder    cache.Decoder    // The decoder to decode binary to cache Data after reading
+}
+
+// newMongoStore returns a new Mongo cache store based on given
+// configuration.
+func newMongoStore(cfg Config) *mongoStore {
+	return &mongoStore{
+		nowFunc:    cfg.nowFunc,
+		db:         cfg.db,
+		collection: cfg.Collection,
+		encoder:    cfg.Encoder,
+		decoder:    cfg.Decoder,
+	}
+}
+
+type item struct {
+	Value interface{}
+}
+
+type cacheFields struct {
+	Data      []byte    `bson:"data"`
+	Key       string    `bson:"key"`
+	ExpiredAt time.Time `bson:"expired_at"`
+}
+
+func (s *mongoStore) Get(ctx context.Context, key string) (interface{}, error) {
+	var fields cacheFields
+	err := s.db.Collection(s.collection).
+		FindOne(ctx, bson.M{"key": key, "expired_at": bson.M{"$gt": s.nowFunc().UTC()}}).Decode(&fields)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, os.ErrNotExist
+		}
+		return nil, errors.Wrap(err, "find")
+	}
+
+	v, err := s.decoder(fields.Data)
+	if err != nil {
+		return nil, errors.Wrap(err, "decode")
+	}
+
+	item, ok := v.(*item)
+	if !ok {
+		return nil, os.ErrNotExist
+	}
+	return item.Value, nil
+}
+
+func (s *mongoStore) Set(ctx context.Context, key string, value interface{}, lifetime time.Duration) error {
+	binary, err := s.encoder(item{value})
+	if err != nil {
+		return errors.Wrap(err, "encode")
+	}
+
+	fields := cacheFields{
+		Data:      binary,
+		Key:       key,
+		ExpiredAt: s.nowFunc().Add(lifetime).UTC(),
+	}
+
+	upsert := true
+	_, err = s.db.Collection(s.collection).
+		UpdateOne(ctx, bson.M{"key": key}, bson.M{"$set": fields}, &options.UpdateOptions{
+			Upsert: &upsert,
+		})
+	if err != nil {
+		return errors.Wrap(err, "upsert")
+	}
+	return nil
+}
+
+func (s *mongoStore) Delete(ctx context.Context, key string) error {
+	_, err := s.db.Collection(s.collection).DeleteOne(ctx, bson.M{"key": key})
+	if err != nil {
+		return errors.Wrap(err, "delete")
+	}
+	return nil
+}
+
+func (s *mongoStore) Flush(ctx context.Context) error {
+	return s.db.Collection(s.collection).Drop(ctx)
+}
+
+func (s *mongoStore) GC(ctx context.Context) error {
+	_, err := s.db.Collection(s.collection).DeleteMany(ctx, bson.M{"expired_at": bson.M{"$lte": s.nowFunc().UTC()}})
+	if err != nil {
+		return errors.Wrap(err, "delete")
+	}
+	return nil
+}
+
+// Options keeps the settings to set up Mongo client connection.
+type Options = options.ClientOptions
+
+// Config contains options for the Mongo cache store.
+type Config struct {
+	// For tests only
+	nowFunc func() time.Time
+	db      *mongo.Database
+
+	// Options is the settings to set up the MongoDB client connection.
+	Options *Options
+	// Database is the database name of the MongoDB.
+	Database string
+	// Collection is the collection name for storing cache Data. Default is "cache".
+	Collection string
+	// Encoder is the encoder to encode cache Data. Default is a Gob encoder.
+	Encoder cache.Encoder
+	// Decoder is the decoder to decode cache Data. Default is a Gob decoder.
+	Decoder cache.Decoder
+}
+
+// Initer returns the cache.Initer for the Mongo cache store.
+func Initer() cache.Initer {
+	return func(ctx context.Context, args ...interface{}) (cache.Cache, error) {
+		var cfg *Config
+		for i := range args {
+			switch v := args[i].(type) {
+			case Config:
+				cfg = &v
+			}
+		}
+
+		if cfg == nil {
+			return nil, fmt.Errorf("config object with the type '%T' not found", Config{})
+		} else if cfg.Database == "" && cfg.db == nil {
+			return nil, errors.New("empty Database")
+		}
+
+		if cfg.db == nil {
+			client, err := mongo.Connect(ctx, cfg.Options)
+			if err != nil {
+				return nil, errors.Wrap(err, "connect database")
+			}
+			cfg.db = client.Database(cfg.Database)
+		}
+
+		if cfg.nowFunc == nil {
+			cfg.nowFunc = time.Now
+		}
+		if cfg.Collection == "" {
+			cfg.Collection = "cache"
+		}
+		if cfg.Encoder == nil {
+			cfg.Encoder = cache.GobEncoder
+		}
+		if cfg.Decoder == nil {
+			cfg.Decoder = func(binary []byte) (interface{}, error) {
+				buf := bytes.NewBuffer(binary)
+				var v item
+				return &v, gob.NewDecoder(buf).Decode(&v)
+			}
+		}
+
+		return newMongoStore(*cfg), nil
+	}
+}
---
+++ mongo/mongo_test.go
@@ -0,0 +1,150 @@
+package mongo
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/flamego/cache"
+	"github.com/flamego/flamego"
+)
+
+func newTestDB(t *testing.T, ctx context.Context) (testDB *mongo.Database, cleanup func() error) {
+	uri := os.Getenv("MONGODB_URI")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		t.Fatalf("Failed to connect to mongo: %v", err)
+	}
+
+	dbname := "flamego-test-cache"
+	err = client.Database(dbname).Drop(ctx)
+	if err != nil {
+		t.Fatalf("Failed to drop test database: %v", err)
+	}
+	db := client.Database(dbname)
+	t.Cleanup(func() {
+		if t.Failed() {
+			t.Logf("DATABASE %s left intact for inspection", dbname)
+			return
+		}
+
+		err = db.Drop(ctx)
+		if err != nil {
+			t.Fatalf("Failed to drop test database: %v", err)
+		}
+	})
+	return db, func() error {
+		if t.Failed() {
+			return nil
+		}
+
+		err = db.Collection("cache").Drop(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func TestMongoStore(t *testing.T) {
+	gob.Register(time.Duration(0))
+
+	ctx := context.Background()
+	db, cleanup := newTestDB(t, ctx)
+	t.Cleanup(func() {
+		assert.NoError(t, cleanup())
+	})
+
+	f := flamego.NewWithLogger(&bytes.Buffer{})
+	f.Use(cache.Cacher(
+		cache.Options{
+			Initer: Initer(),
+			Config: Config{
+				nowFunc: time.Now,
+				db:      db,
+			},
+		},
+	))
+
+	f.Get("/", func(c flamego.Context, cache cache.Cache) {
+		ctx := c.Request().Context()
+
+		assert.NoError(t, cache.Set(ctx, "username", "flamego", time.Minute))
+
+		v, err := cache.Get(ctx, "username")
+		assert.NoError(t, err)
+		username, ok := v.(string)
+		assert.True(t, ok)
+		assert.Equal(t, "flamego", username)
+
+		assert.NoError(t, cache.Delete(ctx, "username"))
+		_, err = cache.Get(ctx, "username")
+		assert.Equal(t, os.ErrNotExist, err)
+
+		assert.NoError(t, cache.Set(ctx, "timeout", time.Minute, time.Hour))
+		v, err = cache.Get(ctx, "timeout")
+		assert.NoError(t, err)
+		timeout, ok := v.(time.Duration)
+		assert.True(t, ok)
+		assert.Equal(t, time.Minute, timeout)
+
+		assert.NoError(t, cache.Set(ctx, "random", "value", time.Minute))
+		assert.NoError(t, cache.Flush(ctx))
+		_, err = cache.Get(ctx, "random")
+		assert.Equal(t, os.ErrNotExist, err)
+	})
+
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.NoError(t, err)
+
+	f.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+}
+
+func TestMongoStore_GC(t *testing.T) {
+	ctx := context.Background()
+	db, cleanup := newTestDB(t, ctx)
+	t.Cleanup(func() {
+		assert.NoError(t, cleanup())
+	})
+
+	now := time.Now()
+	store, err := Initer()(
+		ctx,
+		Config{
+			nowFunc: func() time.Time { return now },
+			db:      db,
+		},
+	)
+	assert.NoError(t, err)
+
+	assert.NoError(t, store.Set(ctx, "1", "1", time.Second))
+	assert.NoError(t, store.Set(ctx, "2", "2", 2*time.Second))
+	assert.NoError(t, store.Set(ctx, "3", "3", 3*time.Second))
+
+	// Read on an expired cache item should remove it
+	now = now.Add(2 * time.Second)
+	_, err = store.Get(ctx, "1")
+	assert.Equal(t, os.ErrNotExist, err)
+
+	// "2" should be recycled
+	assert.NoError(t, store.GC(ctx))
+	_, err = store.Get(ctx, "2")
+	assert.Equal(t, os.ErrNotExist, err)
+
+	// "3" should be returned
+	v, err := store.Get(ctx, "3")
+	assert.NoError(t, err)
+	assert.Equal(t, "3", v)
+}
